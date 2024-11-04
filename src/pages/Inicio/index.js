3519a6e84007a0302b1f594a5d02c432a23c3952
Alexandre <alexandrefaria3012@gmail.com>
2024-11-04 20:01:46 -0300

    Update index.js

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,6 +1,7 @@
 import Rodape from "components/Rodape"
 import Cabecalho from "components/Cabecalho"
 import Banner from "components/Banner"
+import Card from "components/Card"
 import Titulo from "components/Titulo"
 
 const Inicio = () => {
@@ -11,6 +12,7 @@ const Inicio = () => {
             <Titulo>
                 <h1>Um lugar para guardar seus vídeos e filmes!</h1>
             </Titulo>
+            <Card id='1' titulo = 'No Pain No Gain' capa = 'https://thecatapi.com/api/images/get?format-src&type=png'/>
             <Rodape/>
         </>
     )
