e595fae42d832ba29827b3fdd6302c3bd5afcfd8
Alexandre <alexandrefaria3012@gmail.com>
2024-11-04 20:54:16 -0300

    Update index.js

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,9 +1,20 @@
 import styles from './Favoritos.module.css'
+import bannerFavoritos from '../../assets/banner-favoritos.png'
+import Titulo from 'components/Titulo'
+import Card from 'components/Card'
 
 const Favoritos = () => {
     return(
         <>
-            
+            <div>
+                <img src={bannerFavoritos} alt = "banner da página" />
+            </div>
+            <Titulo>
+                <h1>Meus Favoritos!</h1>
+            </Titulo>
+            <section className={styles.container}>
+                <Card id="1" titulo="jcao" capa="https://thecatapi.com/api/images/get?format=src&type=png"/>
+            </section>
         </>
     )
 }
