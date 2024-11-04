1364933f16e6afa03af21e371e090a58a8de73bc
Alexandre <alexandrefaria3012@gmail.com>
2024-11-04 16:28:12 -0300

    Update index.js

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,6 +1,8 @@
+const { default: Cabecalho } = require("components/Cabecalho")
+
 const Inicio = () => {
     return(
-        <h1>ola mundo estou numa page nova</h1>
+        <Cabecalho/>
     )
 }
 
