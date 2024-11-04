e6bac5ceca14462aae505891183e0844668343dd
Alexandre <alexandrefaria3012@gmail.com>
2024-11-04 16:01:44 -0300

    Update index.js

--- src/components/Cabecalho/index.js
+++ src/components/Cabecalho/index.js
@@ -1,3 +1,15 @@
+import { Link } from "react-router-dom"
+import logo from './logo.png'
+import styles from './Cabecalho.module.css'
+
 const Cabecalho = () => {
-    
+    return(
+        <header className={styles.cabecalho}>
+            <Link to="./">
+                <img src={logo} alt="logo do cine tag"></img>
+            </Link>
+        </header>
+    )
 }
+
+export default Cabecalho
