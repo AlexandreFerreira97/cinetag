6788d400eff809656a4138f84d9323334efb8dc1
Alexandre <alexandrefaria3012@gmail.com>
2024-11-04 16:51:35 -0300

    Create index.js

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from './Rodape.module.css'
+
+const Rodape = () => {
+    return(
+        <footer className={styles.rodape}>
+            <h2>Desenvolvido por Alexandre Ferreira De Faria.</h2>
+        </footer>
+    )
+}
+
+export default Rodape
