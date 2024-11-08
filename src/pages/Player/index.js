056b746422318bea4c2cf54fce2f6961fad5a6a0
Alexandre <alexandrefaria3012@gmail.com>
2024-11-08 13:08:44 -0300

    Update index.js

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -6,7 +6,7 @@ import videos from "json/db.json"
 
 const Player = () => {
     const parametros = useParams();
-    const video = videos.find((video) => {return video.id === Number(parametros.id)})
+    const video = videos.find((video) => {return video.id === Number(parametros.id)});
     return(
         <>
             <div>
@@ -15,6 +15,9 @@ const Player = () => {
             <Titulo>
                 <h1>Player</h1>
             </Titulo>
+            <section className={styles.container}>
+                <iframe width="100%" height="100%" src={video.link} title={video.titulo} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </section>
         </>
     )
 }
