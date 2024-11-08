9e5685a1d9c0228a7627b770e802462ed6f28aac
Alexandre <alexandrefaria3012@gmail.com>
2024-11-08 13:17:37 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import FavoritosProvider from "components/contextos/favoritos"
 import Rodape from "components/Rodape"
 import Favoritos from "pages/Favoritos"
 import Inicio from "pages/Inicio"
+import NaoEncontrada from "pages/NaoEncontrada"
 import Player from "pages/Player"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
@@ -17,6 +18,7 @@ const AppRoute = () => {
                         <Route path="/" element={<Inicio/>}></Route>
                         <Route path= "/favoritos" element={<Favoritos/>}></Route>
                         <Route path="/:id" element={<Player/>}></Route>
+                        <Route path="*" element={<NaoEncontrada/>}></Route>
                     </Routes>
                 </FavoritosProvider>
             </Container>
