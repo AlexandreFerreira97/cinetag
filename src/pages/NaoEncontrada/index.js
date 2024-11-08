6ba2fb22250bc819ccac505b0fff828465458356
Alexandre <alexandrefaria3012@gmail.com>
2024-11-08 13:17:34 -0300

    Update index.js

--- src/pages/NaoEncontrada/index.js
+++ src/pages/NaoEncontrada/index.js
@@ -2,7 +2,10 @@ import styles from './NaoEncontrada.module.css'
 
 const NaoEncontrada = () => {
     return(
-        
+        <section className={styles.container}> 
+            <h2>Ops!</h2>
+            <p>O conteúdo que vc procura não foi encontrado!</p>
+        </section>
     )
 }
 
